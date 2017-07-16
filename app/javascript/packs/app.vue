65839db6fe1eb5140437e8708c12fe1c1a1a231d
Pawel Licznar <pawellicznar@gmail.com>
2017-07-17 00:11:01 +0200

    config & articles component

--- app/javascript/packs/app.vue
+++ app/javascript/packs/app.vue
@@ -1,15 +1,21 @@
 <template>
   <div id="app">
     <p>{{ message }}</p>
+    <articles></articles>
   </div>
 </template>
 
 <script>
+import Articles from './components/articles'
+
 export default {
   data: function () {
     return {
       message: "Hello Vue!"
     }
+  },
+  components: {
+    Articles
   }
 }
 </script>
---
+++ app/javascript/packs/components/articles.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="articles">
+    <h3>{{title}}</h3>
+    <input type="text" v-model="title">
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'articles',
+    data () {
+      return {
+        title: "Siema vue z articles component"
+      }
+    }
+  }
+</script>
+
+<style scoped>
+</style>
