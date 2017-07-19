a1622360e9ff1e1e452c1eeb7ced66143e18d55d
Pawel Licznar <pawellicznar@gmail.com>
2017-07-19 21:31:09 +0200

    Add omniauth gem & basic session controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'app', to: 'articles#app', as: 'app'
+  root "articles#app"
   resources :articles
+  get '/auth/:provider/callback', to: 'sessions#create'
 end
