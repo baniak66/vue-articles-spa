1e22769f7000c87c5526e2016aef4165b63559e9
Pawel Licznar <pawellicznar@gmail.com>
2017-08-06 14:14:52 +0200

    Add title field to article

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170729211712) do
+ActiveRecord::Schema.define(version: 20170804210412) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20170729211712) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "readed", default: false
+    t.string "title"
   end
 
   create_table "comments", force: :cascade do |t|
